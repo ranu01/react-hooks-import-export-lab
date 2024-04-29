740a1e7ae2540d2684df50ec00b3a63231d5875a
lewis karanu <lewiskaranu0@gmail.com>
2024-04-29 12:08:32 +0300

    adds

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
+
 
 // export all three variables as named exports
